a96a2f6457bb614bbdf488c66e10df114c37671c
yelhsasunob <qacbonus@tip.edu.ph>
2024-10-02 23:17:01 +0800

    Update LibraryManagementSystem.java

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,9 +1,8 @@
-package schoolman;
 
 class DynamicArray<T> {
-    private final T[] array;
+    private T[] array;
     private int count;
-    private static final int CAPACITY = 100;
+    private static int CAPACITY = 100;
 
     @SuppressWarnings("unchecked")
     public DynamicArray() {
@@ -12,91 +11,64 @@ class DynamicArray<T> {
     }
 
     public void add(T element) {
-        if (count < CAPACITY) {
-            array[count++] = element;
+        if (count == CAPACITY) {
+            growSize();
+        }
+        array[count++] = element;
+    }
+
+    public void add(int index, T element) {
+        if (index >= CAPACITY) {
+            while (index >= CAPACITY) {
+                growSize();
+            }
+        }
+        if (index >= 0 && index < CAPACITY) {
+            if (array[index] == null && element != null) {
+                count++;
+            } else if (array[index] != null && element == null) {
+                count--;
+            }
+            array[index] = element;
         }
     }
 
     public T remove(int index) {
-        if (index >= 0 && index < count) {
+        if (index >= 0 && index < CAPACITY) {
             T removed = array[index];
-            for (int i = index; i < count - 1; i++) {
-                array[i] = array[i + 1];
+            if (removed != null) {
+                array[index] = null;
+                count--;
             }
-            array[--count] = null;
             return removed;
         }
         return null;
     }
 
     public T get(int index) {
-        if (index >= 0 && index < count) {
+        if (index >= 0 && index < CAPACITY) {
             return array[index];
         }
         return null;
     }
 
+    @SuppressWarnings("unchecked")
+    public void growSize() {
+        T[] temp = (T[]) new Object[CAPACITY + 10];
+        System.arraycopy(array, 0, temp, 0, CAPACITY);
+        array = temp;
+        CAPACITY += 10;
+    }
+
     public int size() {
         return count;
     }
-}
 
-class Book {
-    private final String title;
-    private final String author;
-    private final String price;
-
-    public Book(String title, String author, String price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
+    public boolean isEmpty() {
+        return count == 0;
     }
 
-    @Override
-    public String toString() {
-        return "Title: " + title + ", Author: " + author + ", Price: " + price;
-    }
-}
-
-class Library {
-    private final DynamicArray<Book> books;
-
-    public Library() {
-        books = new DynamicArray<>();
-    }
-
-    public void addBook(Book book) {
-        books.add(book);
-    }
-
-    public Book removeBook(int index) {
-        return books.remove(index);
-    }
-
-    public Book getBook(int index) {
-        return books.get(index);
-    }
-
-    public int getTotalBooks() {
-        return books.size();
-    }
-}
-
-public class LibraryManagementSystem {
-    public static void main(String[] args) {
-        Library library = new Library();
-        Book book1 = new Book("Kingdom", "Yusahira Hara", "$10.99");
-        Book book2 = new Book("One Piece", "Eiichiro Oda", "$11.99");
-
-        library.addBook(book1);
-        library.addBook(book2);
-
-        System.out.println("Total books: " + library.getTotalBooks());
-        System.out.println("Book at index 0: " + library.getBook(0));
-        System.out.println("Book at index 1: " + library.getBook(1));
-
-        Book removed = library.removeBook(0);
-        System.out.println("Removed book: " + removed);
-        System.out.println("Total books after removal: " + library.getTotalBooks());
+    public static int capacity() {
+        return CAPACITY;
     }
 }
